85cefed1c8c02769de911571cc004f2d80f6ed5b
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-06-06 18:26:41 +0200

    week7 notes

---
+++ week7/ajax/main.js
@@ -0,0 +1,38 @@
+//  Get DOM elements
+const textButton = document.querySelector('#number');
+const apiButton = document.querySelector('#chuck');
+const outputDiv = document.querySelector('#output');
+
+// Get the URL for Fetch
+const textURL = 'http://numbersapi.com/random';
+const apiURL = 'http://api.chucknorris.io/jokes/random';
+
+// Get Random number fact
+textButton.addEventListener('click', () => {
+    fetch(textURL)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+    if(response.ok) {
+        return response;
+    }
+        throw Error(response.statusText);
+    })
+    .then( response => response.text() )
+    .then( text => outputDiv.innerText = text )
+    .catch( error => console.log('There was an error:', error))
+},false);
+
+// Get the api random Chuck Norris fact
+apiButton = addEventListener('click', () => {
+    fetch(apiURL)
+    .then(response => {
+        outputDiv.innerHTML  = 'Waiting for response...';
+        if (response.ok) {
+            return response;    
+        }
+        throw Error(response,statusText);
+    })
+    .then(response => response.json())
+    .then(data => outputDiv.innerText = data.value)
+    .catch(error => console.log("there was an error:", error))
+}, false);
